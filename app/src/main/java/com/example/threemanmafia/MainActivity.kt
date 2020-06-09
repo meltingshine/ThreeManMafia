4111352b3e20c05723077e4621167f69a81d8b49
meltingshine <meltingshine@gmail.com>
2020-06-10 07:04:05 +0900

    Initial commit

---
+++ app/src/main/java/com/example/threemanmafia/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.threemanmafia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+    override fun setValues() {
+
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
