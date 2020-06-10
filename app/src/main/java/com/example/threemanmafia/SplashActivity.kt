9ac8a704bb324e24517726303f88b461b065476f
meltingshine <meltingshine@gmail.com>
2020-06-10 19:13:31 +0900

    스플래시 만들었따!

--- app/src/main/java/com/example/threemanmafia/SplashActivity.kt
+++ app/src/main/java/com/example/threemanmafia/SplashActivity.kt
@@ -1,11 +1,33 @@
 package com.example.threemanmafia
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import java.lang.Exception
 
-class SplashActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+class SplashActivity : BaseActivity() {
+    override fun setValues() {
+
     }
+
+    override fun setupEvents() {
+
+            Handler().postDelayed({ //delay를 위한 handler
+                val myIntent = Intent(mContext, MainActivity::class.java)
+                startActivity(myIntent)
+                finish()
+            }, 3000)
+
+
+    }
+
+
+override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_splash)
+    setupEvents()
+    setValues()
+}
 }
